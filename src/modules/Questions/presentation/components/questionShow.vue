f1ed376030794e260dc7b4dca3d2850ce319fe63
Min3m <m.abdelmoneam.dev@gmail.com>
2026-05-18 12:21:33 +0300

    build: question Answers Box

--- src/modules/Questions/presentation/components/questionShow.vue
+++ src/modules/Questions/presentation/components/questionShow.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-  import QuestionHeader from '../subComponents/QuestionShow/QuestionHeader.vue';
+  import QuestionAnswers from '../subComponents/QuestionShow/QuestionAnswers.vue';
+import QuestionHeader from '../subComponents/QuestionShow/QuestionHeader.vue';
   import QuestionInfo from '../subComponents/QuestionShow/QuestionInfo.vue';
 </script>
 
@@ -7,5 +8,16 @@
   <div class="question-show-page">
     <QuestionHeader />
     <QuestionInfo />
+    <QuestionAnswers />
   </div>
 </template>
+
+
+<style lang="scss" scoped>
+.question-show-page{
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 1rem;
+}
+</style>
---
+++ src/modules/Questions/presentation/subComponents/QuestionShow/QuestionAnswers.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+  import questionImage from '@/assets/images/question-image.png';
+
+  const answers = ref([
+    {
+      id: 1,
+      text: 'Lungs',
+      img: questionImage,
+      isCorrect: false,
+    },
+    {
+      id: 2,
+      text: 'Heart',
+      img: questionImage,
+      isCorrect: true,
+    },
+    {
+      id: 3,
+      text: 'Kidneys',
+      img: questionImage,
+      isCorrect: false,
+    },
+    {
+      id: 4,
+      text: 'Liver',
+      img: questionImage,
+      isCorrect: false,
+    },
+  ]);
+</script>
+<template>
+  <div class="question-answers-wrapper">
+    <div class="question-answers-list">
+      <div
+        class="question-answer-item"
+        :class="{ correct: answer.isCorrect }"
+        v-for="answer in answers"
+        :key="answer.id"
+      >
+        <div class="answer-content">
+          <label> Answers No : {{ answer.id }}</label>
+          <h2 class="answer">{{ answer.text }}</h2>
+        </div>
+        <div class="answer-image">
+          <img :src="answer.img" :alt="answer.text" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
