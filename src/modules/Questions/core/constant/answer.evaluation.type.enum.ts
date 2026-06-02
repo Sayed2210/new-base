76242c21d01b0a94605f9396722e36f58eb537cb
mahmoudaly66 <mahmoud666aly@gmail.com>
2026-06-02 14:10:33 +0300

    fix question

--- src/modules/Questions/core/constant/answer.evaluation.type.enum.ts
+++
@@ -1,8 +0,0 @@
-export const AnswerEvaluationTypeEnum = {
-  typical : 1,
-  similar: 2,
-  need_correct: 3,
- 
-} as const;
-
-export type AnswerEvaluationTypeEnum = (typeof AnswerEvaluationTypeEnum)[keyof typeof AnswerEvaluationTypeEnum];
--- src/modules/Questions/data/api/FullSubjectTree/full.subject.tree.api.endpoints.ts
+++
@@ -1,7 +0,0 @@
-import { BaseEndpoints } from '@/base/Data/Endpoints/BaseEndpoints';
-
-export class FullSubjectTreeApiEndpoints extends BaseEndpoints {
-  protected readonly prefix = 'dashboard/';
-
-  readonly index = this.url('fetch_full_education_classification_subjects');
-}
